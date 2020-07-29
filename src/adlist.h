a1300118a3e27269fcf96b8a76c1455decee1aa7
liukai <liukai@souche.com>
2020-07-29 21:21:13 +0800

    Feat bloom filter

---
+++ src/adlist.h
@@ -0,0 +1,29 @@
+/* 
+ * adlist.h - 通用双向链表实现
+ * 
+ * 参考自 Redis 4.0.14
+ */
+
+/* 节点 */
+typedef struct listNode {
+    struct listNode *prev;
+    struct listNode *next;
+    void *value;
+} listNode;
+
+/* 链表 */
+typedef struct list {
+    listNode *head;
+    listNode *tail;
+    unsigned long len;
+} list;
+
+
+/* 宏实现函数 */
+#define listLength(l) ((l)->len)
+#define listFirst(l) ((l)->head)
+#define listLast(l) ((l)->tail)
+
+#define listPrevNode(n) ((n)->prev)
+#define listNextNode(n) ((n)->next)
+#define listNodeValue(n) ((n)->value)
